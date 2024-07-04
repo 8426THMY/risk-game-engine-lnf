9ad358eccb2a8d139cfedd7e3dfca0c10ae5cf2a
8426THMY <8426THMY@gmail.com>
2024-07-04 17:46:06 +1000

    Removed unused package.

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -6,5 +6,4 @@ directory=$(realpath $(dirname "$0"))
 python3 -m venv "$directory/.venv"
 source "$directory/.venv/Scripts/activate"
 python -m pip install -e "$directory/risk-shared" -e "$directory/risk-helper" -e "$directory/risk-engine"
-python -m pip install namedpipe
 deactivate
--- setup_env.sh
+++ setup_env.sh
@@ -6,5 +6,4 @@ directory=$(realpath $(dirname "$0"))
 python3 -m venv "$directory/.venv"
 source "$directory/.venv/Scripts/activate"
 python -m pip install "$directory/risk-shared" "$directory/risk-helper" "$directory/risk-engine"
-python -m pip install namedpipe
 deactivate
